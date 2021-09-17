08d6c8e894f6172b8f657bfb3abd86caa56eacf7
jeffnb <jeffnb@pm.me>
2021-09-17 14:38:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import csv
+from copy import copy
+from uuid import uuid4, UUID
+
+from fastapi import FastAPI, Query
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Cereal(BaseModel):
+    uid: UUID = Field(default_factory=uuid4)
+    name: str
+    mfr: str
+    type: str
+    calories: int
+    protein: int
+    sodium: int
+    fiber: float
+    carbo: float
+    sugars: int
+    potass: int
+    vitamins: int
+    shelf: int
+    weight: float
+    cups: float
+    rating: float
+
+# These are just to simulate a data store
+data = []
+def setup():
+    with open("cereal.csv", "r") as f:
+        reader = csv.DictReader(f)
+        for idx, line in enumerate(reader):
+            data.append(Cereal(**line))
+
+
+@app.get("/cereals/")
+def get_all(q: str = None, calories: int = None):
+    return data
+
+setup()
+
